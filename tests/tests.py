b6e43adb821813ddbeaefc8a888d1c2291581714
Thomas Preston <thomasmarkpreston@gmail.com>
2013-12-09 13:41:02 +0000

    added blocking test

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,6 @@
 import unittest
 import lirc
+import time
 
 
 class TestNextCode(unittest.TestCase):
@@ -10,6 +11,25 @@ class TestNextCode(unittest.TestCase):
     def test_nextcode(self):
         print("Press 1 on your remote.")
         self.assertEqual(lirc.nextcode(), ["horses"])
+
+    def tearDown(self):
+        lirc.deinit()
+
+
+class TestNonBlocking(unittest.TestCase):
+    """Tests the non blocking setting."""
+    def setUp(self):
+        lirc.init("lirctest", "tests/lircrc.test", blocking=False)
+
+    def test_nonblocking_nextcode(self):
+        print("Press 1 on your remote.")
+        start_time = time.time()
+        end_time = start_time + 5  # 5 seconds in the future
+        pressed = False
+        while not pressed or time.time() < end_time:
+            if lirc.nextcode() == ["horses"]:
+                pressed = True
+        self.assertTrue(pressed)
 
     def tearDown(self):
         lirc.deinit()
